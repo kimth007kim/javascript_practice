b0f3fada4fbae16fbecfcf5a48e1e327ba2bd015
KyungDong <kimth007kim@gmail.com>
2020-04-21 17:43:49 +0900

    #2.3 Changing Color 와 #2.4 Brush Size 완료

--- vanillagame/app.js
+++ vanillagame/app.js
@@ -1,5 +1,8 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
 canvas.width = 700;
 canvas.height = 700;
@@ -8,6 +11,7 @@ ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function stopPainting() {
   painting = false;
@@ -21,17 +25,32 @@ function onMouseMove(event) {
   const y = event.offsetY;
   if (!painting) {
     // !painting은 painting===false와 똑같은 말이다.
-    console.log("creating path in ", x, y);
     ctx.beginPath();
     ctx.moveTo(x, y);
   } else {
-    console.log("creating line in ", x, y);
     ctx.lineTo(x, y);
     ctx.stroke();
   }
 }
-function onMouseDown(event) {
-  painting = true;
+
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+}
+
+function handleRangeChange(event) {
+  const size = event.target.value;
+  ctx.lineWidth = size;
+}
+
+function handleModeClick() {
+  if (filling === true) {
+    filling = false;
+    mode.innerText = "Fill";
+  } else {
+    filling = true;
+    mode.innerText = "Paint";
+  }
 }
 
 if (canvas) {
@@ -39,4 +58,15 @@ if (canvas) {
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
+}
+
+Array.from(colors).forEach((color) =>
+  color.addEventListener("click", handleColorClick)
+);
+
+if (range) {
+  range.addEventListener("input", handleRangeChange);
+}
+if (mode) {
+  mode.addEventListener("click", handleModeClick);
 }
