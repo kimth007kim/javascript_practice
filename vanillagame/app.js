730061256d15f55aa088ac5b04469bf0e239ea17
kimth007kim <kimth007kim@naver.com>
2020-04-23 17:48:29 +0900

    #2.6 Saving the Image 완료

--- vanillagame/app.js
+++ vanillagame/app.js
@@ -1,14 +1,36 @@
+// const { app, BrowserWindow } = require("electron");
+// const path = require("path");
+// function createWindow() {
+//   // 브라우저 창을 생성합니다.
+//   let win = new BrowserWindow({
+//     width: 800,
+//     height: 600,
+//     webPreferences: {
+//       nodeIntegration: true,
+//     },
+//   });
+
+//   // 그리고 앱의 index.html를 로드합니다.
+//   win.loadFile("index.html");
+// }
+
+// app.whenReady().then(createWindow);
+
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
 const INITIAL_COLOR = "#2c2c2c";
 const CANVAS_SIZE = 700;
 
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
 
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
@@ -64,6 +86,18 @@ function handleCanvasClick() {
     ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
   }
 }
+function handleCM(event) {
+  event.preventDefault();
+}
+function handleSaveClick() {
+  const image = canvas.toDataURL();
+  const link = document.createElement("a");
+  console.log(image);
+
+  link.href = image;
+  link.download = "PaintJS";
+  link.click();
+}
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
@@ -71,6 +105,7 @@ if (canvas) {
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
   canvas.addEventListener("click", handleCanvasClick);
+  canvas.addEventListener("contextmenu", handleCM);
 }
 
 Array.from(colors).forEach((color) =>
@@ -82,4 +117,7 @@ if (range) {
 }
 if (mode) {
   mode.addEventListener("click", handleModeClick);
+}
+if (saveBtn) {
+  saveBtn.addEventListener("click", handleSaveClick);
 }
