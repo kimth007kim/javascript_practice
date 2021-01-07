cbad62d38012d345dd22b68569cfb4c8df803c5d
kimth007kim <kimth007kim@naver.com>
2021-01-07 15:32:17 +0900

    11 비동기 처리의 시작 콜백 이해하기,콜백 지옥 체험

---
+++ 드림코딩 by 엘리/async/callback.js
@@ -0,0 +1,67 @@
+"use strict";
+
+console.log("1");
+setTimeout(() => console.log("2"), 1000);
+console.log("3");
+
+//동기 콜백
+
+function printImmediately(print) {
+  print();
+}
+printImmediately(() => console.log("hello"));
+
+//비동기 콜백
+
+function printWithDelay(print, timeout) {
+  setTimeout(print, timeout);
+}
+printWithDelay(() => console.log("async callback"), 2000);
+
+class UserStorage {
+  loginUser(id, password, onSuccess, onError) {
+    setTimeout(() => {
+      if (
+        (id === "ellie" && password === "dream") ||
+        (id === "coder" && password === "academy")
+      ) {
+        onSuccess(id);
+      } else {
+        onError(new Error("not found"));
+      }
+    }, 2000);
+  }
+  getRoles(user, onSuccess, onError) {
+    setTimeout(() => {
+      if (user === "ellie") {
+        onSuccess({ name: "ellie", role: "admin" });
+      } else {
+        onError(new Error("np access"));
+      }
+    }, 1000);
+  }
+}
+
+const userStorage = new UserStorage();
+const id = prompt("enter your id");
+const password = prompt("enter your password");
+userStorage.loginUser(
+  id,
+  password,
+  (user) => {
+    userStorage.getRoles(
+      user,
+      (userWithRole) => {
+        alert(
+          `Hello ${userWithRole.name}, you have a ${userWithRole.role} role`
+        );
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  },
+  (error) => {
+    console.log(error);
+  }
+);
